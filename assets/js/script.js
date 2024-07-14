f4fd54daa7f369d8907af2debb575f6f089131ac
yash <yporwal333@gmail.com>
2024-07-14 08:47:48 +0530

    js formatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,12 @@
 $(function () {
-  // alert("yash");
   if (window.matchMedia("(max-width: 991.99px)").matches) {
-    // $(".header .header-desktop .main_menu").slideToggle();
     $(".header .menu-btn").on("click", function () {
       $(".header .header-desktop .main_menu").slideToggle();
     });
-    // $('.menu-btn').on ("click",function(){
-    //   $(".menu-btn img").toggleAttrVal('src', "assets/images/close-icon.svg","assets/images/menu-icon.svg");
-    // });
 
-    $('.menu-btn').click(function() {
+    $(".menu-btn").click(function () {
       $(this).toggleClass("close");
     });
   }
-  $('.js-tilt').tilt({
-  });
+  $(".js-tilt").tilt({});
 });
